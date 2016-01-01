48194324aeb02887394968694c5c909fd582b27d
Damanpreet <daman.4880@gmail.com>
2016-01-01 16:47:22 +0530

    Added file to use Instagram Credentilals in app

---
+++ app/secret.py
@@ -0,0 +1,8 @@
+
+secrets = {
+"client_id" :"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+"redirect_uri":"http://picsaver.pythonanywhere.com/instagram_callback/",
+"client_secret":"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+secret_key = "User defined Secret Key"
